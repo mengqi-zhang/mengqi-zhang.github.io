3c73ce8578e5988f86c26f5bb3e929a82c9a1330
mengqizhang <zhangmengqi3@beisen.com>
2016-04-29 09:45:54 +0800

    push

--- js/getUsermedia.js
+++ js/getUsermedia.js
@@ -1,31 +1,29 @@
 window.onmessage = function(ev){
   if(ev.data == 'photo'){
-    alert(1)
      openVideo();
   }else if(ev.data == 'startRecording'){
-    alert(2)
     startRecording()
   }else if(ev.data == 'stopRecording'){
-    alert(3)
     stopRecording()
+  }else if(ev.data == 'takephoto'){
+    iClick()
   }
 }
+openVideo()
 var video = document.querySelector('video');
+var canvas = window.canvas = document.querySelector('canvas');
+var imgdata= document.getElementById("imgdata");
+var button = document.querySelector('button');
+var oDownload = document.getElementById("download");
+var imgStr = '';
+var recordVideo = ''
+canvas.width = 480;
+canvas.height = 284;
 function openVideo(){
-      var canvas = window.canvas = document.querySelector('canvas');
-      var imgdata= document.getElementById("imgdata");
-      var button = document.querySelector('button');
-      var oDownload = document.getElementById("download");
-      var imgStr = '';
-      var recordVideo = ''
-      canvas.width = 480;
-      canvas.height = 360;
-
       var constraints = {
         audio: true,
         video: true
       };
-
       function successCallback(stream) {
         window.stream = stream; 
         video.srcObject = stream;
@@ -38,26 +36,20 @@ function openVideo(){
         console.log('navigator.getUserMedia error: ', error);
       }
       navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-      navigator.getUserMedia(constraints, successCallback, errorCallback);
-
-
-      function convertCanvasToImage(oCanvas) {
-        imgdata.value=oCanvas.toDataURL();
-
-      }
-      function iClick() {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-        convertCanvasToImage(canvas);
-        parent.window.postMessage(imgdata.value,'http://localhost:8881/main.html')
-      };
-      var timer = setInterval(function(){
-        iClick()
-      },5000)
+      navigator.getUserMedia(constraints, successCallback, errorCallback);      
       
 }
-
+function iClick() {
+    video.style.display = 'none';
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    convertCanvasToImage(canvas);
+    parent.window.postMessage(imgdata.value,'http://localhost:8881/main.html');
+};
+function convertCanvasToImage(oCanvas) {
+    imgdata.value=oCanvas.toDataURL();
+}    
 var recordVideo;
 function startRecording() {
     navigator.getUserMedia({
